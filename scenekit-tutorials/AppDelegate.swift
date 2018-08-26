dcfab8d27795957e3ccf963120f0950214254594
Michael Wu <michael-yx-wu@users.noreply.github.com>
2018-08-26 20:06:12 +0100

    Initial Commit

---
+++ scenekit-tutorials/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  scenekit-tutorials
+//
+//  Created by Michael Wu on 8/26/18.
+//  Copyright © 2018 michaelwu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
