625ca0395c592c3e3883dc95231ad2861fd5ba07
Michael Wu <michael-yx-wu@users.noreply.github.com>
2018-10-14 15:54:45 +0100

    Add end code for starting out

--- scenekit-tutorials/AppDelegate.swift
+++ scenekit-tutorials/AppDelegate.swift
@@ -7,21 +7,34 @@
 //
 
 import Cocoa
+import SceneKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet weak var window: SCNView!
+    override func awakeFromNib() {
+        let scene = SCNScene()
+        let boxSide: CGFloat = 10.0
+        let box = SCNBox(width: boxSide, height: boxSide, length: boxSide, chamferRadius: 0)
 
-    @IBOutlet weak var window: NSWindow!
+        let boxNode = SCNNode(geometry: box)
+        scene.rootNode.addChildNode(boxNode)
 
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3Make(0, 10, 20)
+        cameraNode.rotation = SCNVector4Make(1, 0, 0, -atan2(10.0, 20.0))
+        scene.rootNode.addChildNode(cameraNode)
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let lightBlueColor = NSColor(calibratedRed: 4.0/255.0, green: 120.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+        let light = SCNLight()
+        light.type = SCNLight.LightType.directional
+        light.color = lightBlueColor
+        let lightNode = SCNNode()
+        lightNode.light = light
+        cameraNode.addChildNode(lightNode)
+
+        window.scene = scene
+
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
-
